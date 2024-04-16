d664b8e3397c9d00d63e54d1f170776c2e690fcf
nufhere <tuandaklak@gmail.com>
2024-04-16 10:52:52 +0700

    fist cript: x o simple text

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+var currentPlayer = 'X';
+$(document).ready(function () {
+    //Hàm ready đảm bảo những mã trong này được chạy khi
+    //mọi thành phần của DOM đã được load xong
+    $('.cell').click(function (e) { 
+        console.log('cell clicked');
+        $(this).text(currentPlayer);
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    });
 
+});
   
