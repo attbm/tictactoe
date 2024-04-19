3a5de7448120f0cbe91698d7d74bc4b5d92f7ca0
nufhere <tuandaklak@gmail.com>
2024-04-19 10:44:49 +0700

    xu ly hang doc va hang ngang

--- functions.js
+++ functions.js
@@ -117,22 +117,122 @@ function renderBoard(gameState) {
    * @returns gameState sau khi thay đổi
    */
   function computerPlay(gameState) {
-    const availableCells = [];
-    // Lặp qua mỗi ô trên bàn cờ và lưu trữ vị trí các ô trống
+      return addOTurnForMaxOCount(gameState);
+  } //end computerPlay
+
+  /**
+   * Hàm cho O đi thêm một nước sao cho số lượng trên hàng dọc là lớn nhất
+   * @param {*} gameState 
+   * @returns 
+   */
+  function addOTurnForMaxHorizontalO(gameState) {
+    let maxRow = -1;
+    let maxOCount = -1;
+  
+    // Tính số lượng O trên mỗi hàng ngang và chỉ tính nếu không có X
     for (let i = 0; i < gameState.length; i++) {
+      let oCount = 0;
       for (let j = 0; j < gameState[i].length; j++) {
-        if (gameState[i][j] === '') {
-          availableCells.push({ row: i, col: j });
+        if (gameState[i][j] === 'O') {
+          oCount++;
+        } else if (gameState[i][j] === 'X') {
+          oCount = -1; // Nếu có X, không tính hàng này
+          break;
+        }
+      }
+      if (oCount > maxOCount) {
+        maxOCount = oCount;
+        maxRow = i;
+      }
+    }
+  
+    // Thêm O vào hàng ngang có số O lớn nhất và không có X
+    if (maxRow !== -1) {
+      for (let j = 0; j < gameState[maxRow].length; j++) {
+        if (gameState[maxRow][j] === '') {
+          gameState[maxRow][j] = 'O';
+          break;
         }
       }
     }
   
-    // Chọn một ô trống ngẫu nhiên
-    const randomIndex = Math.floor(Math.random() * availableCells.length);
-    const chosenCell = availableCells[randomIndex];
+    return gameState;
+  }
+
+  function addOTurnForMaxVerticalO(gameState) {
+    let maxCol = -1;
+    let maxOCount = -1;
   
-    // Điền 'O' vào ô đã chọn
-    gameState[chosenCell.row][chosenCell.col] = 'O';
+    // Tạo mảng để lưu trữ số lượng O trên mỗi cột và kiểm tra không có X
+    const oCounts = Array.from({ length: gameState[0].length }, () => 0);
+    for (let j = 0; j < gameState[0].length; j++) {
+      for (let i = 0; i < gameState.length; i++) {
+        if (gameState[i][j] === 'O') {
+          oCounts[j]++;
+        } else if (gameState[i][j] === 'X') {
+          oCounts[j] = -1; // Nếu có X, không tính cột này
+          break;
+        }
+      }
+    }
+  
+    // Tìm cột có số O lớn nhất và không có X
+    for (let j = 0; j < oCounts.length; j++) {
+      if (oCounts[j] > maxOCount) {
+        maxOCount = oCounts[j];
+        maxCol = j;
+      }
+    }
+  
+    // Thêm O vào cột có số O lớn nhất và không có X
+    if (maxCol !== -1) {
+      for (let i = 0; i < gameState.length; i++) {
+        if (gameState[i][maxCol] === '') {
+          gameState[i][maxCol] = 'O';
+          break;
+        }
+      }
+    }
   
     return gameState;
   }
+
+  function addOTurnForMaxOCount(gameState) {
+    // Sao chép mảng trạng thái để tránh thay đổi mảng ban đầu
+    const horizontalGameState = copyGameState(gameState);
+    const verticalGameState = copyGameState(gameState);
+  
+    // Thêm O vào hàng ngang và hàng dọc
+    const horizontalResult = addOTurnForMaxHorizontalO(horizontalGameState);
+    const verticalResult = addOTurnForMaxVerticalO(verticalGameState);
+  
+    // Đếm số O trên bàn cờ sau khi thêm vào hàng ngang và hàng dọc
+    let horizontalOCount = countO(horizontalResult);
+    let verticalOCount = countO(verticalResult);
+  
+    // Chọn kết quả tốt nhất và trả về
+    if (horizontalOCount > verticalOCount) {
+      return horizontalResult;
+    } else {
+      return verticalResult;
+    }
+  }
+  
+  // Hàm để sao chép mảng trạng thái
+  function copyGameState(gameState) {
+    return gameState.map(row => [...row]);
+  }
+  
+  // Hàm đếm số lượng 'O' trên bàn cờ
+  function countO(gameState) {
+    let oCount = 0;
+    for (let i = 0; i < gameState.length; i++) {
+      for (let j = 0; j < gameState[i].length; j++) {
+        if (gameState[i][j] === 'O') {
+          oCount++;
+        }
+      }
+    }
+    return oCount;
+  }
+  
